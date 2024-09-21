59b67f89d05229b8aa32bc0ab85ef6f67ee19773
Son Nguyen <nguyenminhson20051129@gmail.com>
2024-09-21 00:33:56 -0400

    add test file

--- translation_tool/sign_language_translation_tool/tests.py
+++ translation_tool/sign_language_translation_tool/tests.py
@@ -1,17 +1,3 @@
-import requests
+from django.test import TestCase
 
-# URL of the API endpoint
-url = 'http://0.0.0.0:8000/sign_language/translate/'
-
-# JSON data to be sent in the POST request
-data = {
-    'text': 'Hello'
-}
-
-# Sending the POST request
-response = requests.post(url, json=data)
-
-# Print the response status code and JSON response
-print(response.status_code)
-print(response.json()['message'])
-print(response.json()['translated_text'])
+# Create your tests here.
